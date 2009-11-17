8d84000c16c07cff5b0eab35131ab35bfc9e2afc
silentbalanceyh <silentbalanceyh@e528dd6c-a225-11de-a69d-91aafc7ba57a>
2009-11-17 17:04:18 +0000

    Calculate

---
+++ CalculateAverage/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
+CND_ARTIFACT_NAME_Debug=calculateaverage
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/calculateaverage
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
+CND_PACKAGE_NAME_Debug=calculateaverage.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/calculateaverage.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
+CND_ARTIFACT_NAME_Release=calculateaverage
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/calculateaverage
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
+CND_PACKAGE_NAME_Release=calculateaverage.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/calculateaverage.tar

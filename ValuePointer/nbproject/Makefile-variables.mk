8eddcba3247cca84d42897a07e0e7f423d7f8120
silentbalanceyh <silentbalanceyh@e528dd6c-a225-11de-a69d-91aafc7ba57a>
2009-11-17 08:39:20 +0000

    Value Pointer

---
+++ ValuePointer/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
+CND_ARTIFACT_NAME_Debug=valuepointer
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/valuepointer
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
+CND_PACKAGE_NAME_Debug=valuepointer.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/valuepointer.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
+CND_ARTIFACT_NAME_Release=valuepointer
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/valuepointer
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
+CND_PACKAGE_NAME_Release=valuepointer.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/valuepointer.tar
